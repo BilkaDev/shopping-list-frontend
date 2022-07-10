7f7b5e5f858870c5f09ddf6069e2d11a5a40d849
Marcin Bilka <bilkadev@gmail.com>
2022-07-10 14:39:13 +0200

    Added HttpHooks.

---
+++ src/common/hooks/http-hook.ts
@@ -0,0 +1,49 @@
+import {useCallback, useState} from "react";
+import {apiUrl} from "../../config/api";
+
+export type ReqBody = (
+    | FormData
+    | null
+    )
+
+export const useHttpClient = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState<null | string>();
+
+
+    let statusError = 500;
+    const sendRequest = useCallback(
+        async (
+            url: string,
+            method = 'GET',
+            body: ReqBody = null,
+            headers = {}
+        ) => {
+            try {
+                setIsLoading(true);
+                const response = await fetch(`${apiUrl}${url}`, {
+                    method,
+                    headers,
+                    body: body && {body: body instanceof FormData ? body : JSON.stringify(body)}.body,
+                });
+
+                const responseData = await response.json();
+                if (!response.ok) {
+                    statusError = responseData;
+                    throw new Error(responseData.message);
+                }
+                setIsLoading(false);
+
+                return responseData;
+            } catch (e: any) {
+                setError(statusError === 500 ? 'Sorry, please try again later' : e.message);
+                setIsLoading(false);
+
+                throw e;
+            }
+        }, []);
+    const clearError = () => {
+        setError(null);
+    };
+    return {isLoading, error, sendRequest, setError, clearError};
+};
