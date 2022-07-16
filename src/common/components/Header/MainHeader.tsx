f1ce93fa9b889d81bad31153599f0d07f61f3125
Marcin Bilka <bilkadev@gmail.com>
2022-07-16 12:07:07 +0200

    Added navlink to header.

--- src/common/components/Header/MainHeader.tsx
+++ src/common/components/Header/MainHeader.tsx
@@ -1,9 +1,12 @@
-import './MainHeader.css'
+import {NavLink} from "react-router-dom";
+import './MainHeader.css';
+
 export const MainHeader = () => {
 
     return (
         <header className="MainHeader">
-            <h1>Lista zakupów</h1>
+            <NavLink to={'/'}><h1>Lista zakupów</h1>
+            </NavLink>
         </header>
-    )
-}
+    );
+};
