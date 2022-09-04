d06fc3d61c80b1c7b4fe43e5670ab65a1b910e08
BilkaDev <bilkadev@gmail.com>
2022-09-04 23:50:03 +0200

    clear code.

--- src/products/pages/Products.tsx
+++ src/products/pages/Products.tsx
@@ -1,19 +1,16 @@
 import React from "react";
 import { ProductsList } from "../components/ProductsList";
 import { AddProduct } from "../components/AddProduct";
-import "./Products.css";
-import { Box, VStack } from "@chakra-ui/react";
 import { Section } from "../../common/components/UiElements/Section";
 
 export const Products = () => {
 
     return (
-            <Section>
-                <h2>Add product</h2>
-                <AddProduct />
-                <h2>Product list</h2>
-                <ProductsList />
-            </Section>
+        <Section>
+            <h2>Add product</h2>
+            <AddProduct />
+            <h2>Product list</h2>
+            <ProductsList />
+        </Section>
     );
 };
-
