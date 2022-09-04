373e7ad211597418e59cc5de532a07f14b561882
BilkaDev <bilkadev@gmail.com>
2022-09-04 19:23:20 +0200

    Change style in SelectProductCategory

--- src/common/components/FormElements/SelectProductCategory.tsx
+++ src/common/components/FormElements/SelectProductCategory.tsx
@@ -1,38 +1,43 @@
-import React, {ChangeEvent, useEffect, useState} from 'react';
-import {ProductCategory} from 'interfaces'
+import React, { ChangeEvent, useEffect, useState } from "react";
+import { ProductCategory } from "interfaces";
+import { Select } from "@chakra-ui/react";
 
 interface Props {
     onInput: (id: string, value: number, isValid: boolean) => void;
     initialValue?: number;
 }
 
-export const SelectProductCategory = (props:Props) => {
-    const [selectCategory,setSelectCategory] = useState<ProductCategory>(props.initialValue || 0)
-    const {onInput} = props
+export const SelectProductCategory = (props: Props) => {
+    const [selectCategory, setSelectCategory] = useState<ProductCategory>(props.initialValue || 0);
+    const { onInput } = props;
 
-    const entries = Object.entries(ProductCategory)
-    const category = []
+    const entries = Object.entries(ProductCategory);
+    const category = [];
     for (const key of entries) {
-        if(typeof key[1] === 'number'){
-            const option = <option key={key[1]} value={key[1]}>{key[0]}</option>
-            category.push(option)
+        if (typeof key[1] === "number") {
+            const option = <option key={key[1]} value={key[1]}>{key[0]}</option>;
+            category.push(option);
         }
     }
-    
-    useEffect(()=>{
-        onInput('category',selectCategory,true)
-    },[onInput,selectCategory])
 
-    function changeHandler(e:ChangeEvent<HTMLSelectElement>) {
-        const value = Number(e.target.value)
-        setSelectCategory(value)
+    useEffect(() => {
+        onInput("category", selectCategory, true);
+    }, [onInput, selectCategory]);
+
+    function changeHandler(e: ChangeEvent<HTMLSelectElement>) {
+        const value = Number(e.target.value);
+        setSelectCategory(value);
     }
 
     return (
-        <label><p>Dział:</p>
-            <select onChange={changeHandler} value={selectCategory}>
+        <label>
+            <p>Category:</p>
+            <Select color="var(--dark)"
+                    bgColor="var(--white)"
+                    onChange={changeHandler}
+                    value={selectCategory}>
                 {category}
-            </select>
+            </Select>
         </label>
     );
 };
