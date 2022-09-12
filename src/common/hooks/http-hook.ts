6b1aa75c41de377f42f29554d57a33550d7ab551
Marcin Biłka <98228475+BilkaDev@users.noreply.github.com>
2022-09-12 14:27:39 +0200

    Clear code in http-hook.

--- src/common/hooks/http-hook.ts
+++ src/common/hooks/http-hook.ts
@@ -42,7 +42,6 @@ export const useHttpClient = () => {
                 const responseData = await response.json();
                 setIsLoading(false);
                 if (!responseData.isSuccess) {
-                    console.log(responseData);
                     setError(responseData.message);
                     return responseData;
                 }
