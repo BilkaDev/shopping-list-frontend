56a01df5c2b61cead3bb7104a2ad83447e6280cd
Marcin Biłka <98228475+BilkaDev@users.noreply.github.com>
2022-09-14 14:51:01 +0200

    refractories code AddItem and ItemsList.

--- src/lists/components/ItemInList/AddItem.tsx
+++ src/lists/components/ItemInList/AddItem.tsx
@@ -1,29 +1,33 @@
-import React, {useState} from 'react';
-import {Input} from "../../../common/components/FormElements/Input";
-import {VALIDATOR_MAX, VALIDATOR_MAXLENGTH, VALIDATOR_MINLENGTH} from "../../../common/utils/validators";
-import {useForm} from "../../../common/hooks/form-hook";
-import {useHttpClient} from "../../../common/hooks/http-hook";
-import {useDispatch} from "react-redux";
-import {SearchProduct} from "./SearchProduct";
-import {useParams} from "react-router-dom";
-import {CreateProductRequest, CreateItemInListRequest, GetItemInList, GetProductResponse} from 'interfaces';
-import {addProductAction} from "../../../common/Redux/actions/product";
-import {addItemToList} from "../../../common/Redux/actions/list";
+import React, { useState } from "react";
+import { Input } from "../../../common/components/FormElements/Input";
+import { VALIDATOR_MAX, VALIDATOR_MAXLENGTH, VALIDATOR_MINLENGTH } from "../../../common/utils/validators";
+import { useForm } from "../../../common/hooks/form-hook";
+import { useHttpClient } from "../../../common/hooks/http-hook";
+import { useDispatch } from "react-redux";
+import { SearchProduct } from "./SearchProduct";
+import { useParams } from "react-router-dom";
+import { CreateProductRequest, CreateItemInListRequest, GetItemInList, GetProductResponse } from "interfaces";
+import { addProductAction } from "../../../common/Redux/actions/product";
+import { addItemToList } from "../../../common/Redux/actions/list";
+import { Button, VStack } from "@chakra-ui/react";
+import { InfoModal } from "../../../common/components/UiElements/InfoModal";
+import { LoadingSpinner } from "../../../common/components/UiElements/LoadingSpinner";
+import { SuccessfullyBox } from "../../../common/components/UiElements/SuccessfullyBox";
 
 
 export const AddItem = () => {
     const [isSuccess, setIsSuccess] = useState(false);
-    const {formState, inputHandler, setFormData, selectHandler} = useForm({
-            name: {isValid: false, value: ""},
-            count: {isValid: false, value: 0},
-            weight: {isValid: false, value: 0}
+    const { formState, inputHandler, setFormData, selectHandler } = useForm({
+            name: { isValid: false, value: "" },
+            count: { isValid: false, value: 0 },
+            weight: { isValid: false, value: 0 }
         }, false
     );
     const [product, setProduct] = useState<GetProductResponse>();
-    const {isLoading, error, sendRequest, clearError, setError} = useHttpClient();
+    const { isLoading, error, sendRequest, clearError, setError } = useHttpClient();
     const dispatch = useDispatch();
-    const {id} = useParams();
-    const userId = 'user1';
+    const { id } = useParams();
+    const userId = "user1";
 
 
     const addItemToListRequest = async (e: React.FormEvent) => {
@@ -32,32 +36,32 @@ export const AddItem = () => {
 
         // Create product if product not found
         let newProduct: GetProductResponse | undefined = undefined;
-        let newItem: CreateItemInListRequest | undefined = undefined
+        let newItem: CreateItemInListRequest | undefined = undefined;
         if (!product) {
             const newProductReq: CreateProductRequest = {
                 name: formState.inputs.name.value,
                 category: Number(formState.inputs.category.value),
                 userId,
             };
-            const resProduct = await sendRequest('/product', 'POST', newProductReq, {
-                'Content-Type': 'application/json',
+            const resProduct = await sendRequest("/product", "POST", newProductReq, {
+                "Content-Type": "application/json",
             });
             if (!resProduct.isSuccess) {
-                return setError("Dodawanie produktu nie powiodło się, sprawdź nazwe produktu (nazwa nie może się powtarzać)");
+                return setError("Adding a product failed, check the product name (the name must not repeat)");
             }
             newProduct = {
                 ...newProductReq,
                 id: resProduct.id,
             };
             dispatch(addProductAction(newProduct));
-            newItem ={
+            newItem = {
                 itemId: resProduct.id,
                 count: Number(formState.inputs.count.value),
                 weight: Number(formState.inputs.weight.value),
                 listId: id,
             };
         } else {
-            newItem ={
+            newItem = {
                 itemId: product.id,
                 count: Number(formState.inputs.count.value),
                 weight: Number(formState.inputs.weight.value),
@@ -66,102 +70,94 @@ export const AddItem = () => {
         }
 
 
-        const res: any = await sendRequest('/list/item', 'POST', newItem, {
-            'Content-Type': 'application/json',
+        const res: any = await sendRequest("/list/item", "POST", newItem, {
+            "Content-Type": "application/json",
         });
 
         if (!res.isSuccess) {
-            return setError("Dodawanie produktu do listy nie powiodło się.");
+            return setError("Adding a product to the list failed.");
         }
 
         const newItemTEST: GetItemInList = {
             id: res.id,
-           ...newItem,
+            ...newItem,
             product: (product || newProduct) as GetProductResponse,
         };
 
-        dispatch(addItemToList(newItemTEST))
+        dispatch(addItemToList(newItemTEST));
         setIsSuccess(true);
-        inputHandler('name', '', false);
+        inputHandler("name", "", false);
     };
 
     function exitErrorHandler() {
         setFormData({
-            name: {isValid: false, value: ""},
-            productId: {isValid: true, value: ""},
-            count: {isValid: false, value: 0},
-            weight: {isValid: false, value: 0}
+            name: { isValid: false, value: "" },
+            productId: { isValid: true, value: "" },
+            count: { isValid: false, value: 0 },
+            weight: { isValid: false, value: 0 }
         }, false);
         clearError();
     }
 
-
-    //@TODO improve text appearance
-    //@TODO Add product btn
     if (isSuccess) {
         return (
-            <>
-                <p>Dodanie produktu do listy powiodło się.</p>
-                <button onClick={() => setIsSuccess(false)}>Dodaj kolejny produkty</button>
-            </>
+            <SuccessfullyBox text="Adding the product was successful." setIsSuccess={setIsSuccess}/>
         );
     }
 
-    //@TODO fix the appearance of an error or add a modal
     return (
         <>
-            {error && (<>
-                    <p>{error}</p>
-                    <button onClick={exitErrorHandler}>Zamknij</button>
-                </>
-            )}
-            {isLoading && <p>Loading</p>}
-            {!isLoading && !error && <form onSubmit={addItemToListRequest}>
-                <Input
-                    label="Nazwa"
-                    id="name"
-                    placeholder="Nazwa produktu"
-                    errorText="Nazwa produktu jest wymagana (min. 2 znaki max. 100)."
-                    validators={[VALIDATOR_MINLENGTH(2), VALIDATOR_MAXLENGTH(100)]}
-                    onInput={inputHandler}
-                />
-                <SearchProduct
-                    product={product}
-                    setProduct={setProduct}
-                    onInputHandler={inputHandler}
-                    onSelectHandler={selectHandler}
-                    name={formState.inputs.name.value}
-                />
-                <Input
-                    label="Ilość"
-                    id="count"
-                    placeholder="Ilość"
-                    errorText="Maksymalna ilość 1000"
-                    validators={[VALIDATOR_MAX(1000)]}
-                    initialValue={"0"}
-                    initialValid={true}
-                    onInput={inputHandler}
-                    type="number"
-                    min="0"
-                    max="1000"
-                />
-                <Input
-                    label="Waga"
-                    id="weight"
-                    placeholder="Waga"
-                    errorText="Maksymalna waga 1000000"
-                    validators={[VALIDATOR_MAX(1000000)]}
-                    onInput={inputHandler}
-                    initialValid={true}
-                    initialValue={"0"}
-                    type="number"
-                    min="0"
-                    max="1000000"
-
-                />
-                <button disabled={!formState.isValid}>Dodaj do listy</button>
-            </form>}
-
+            {error &&
+                <InfoModal message={error} isError onClose={exitErrorHandler} title={"Failed!"}/>}
+            {isLoading && <LoadingSpinner/>}
+            {!isLoading && !error &&
+                <form onSubmit={addItemToListRequest}>
+                    <VStack spacing={4} align="flex-start">
+                        <Input
+                            label="Name:"
+                            id="name"
+                            placeholder="Product name"
+                            errorText="Product name is required (min. 2 characters max. 100)."
+                            validators={[VALIDATOR_MINLENGTH(2), VALIDATOR_MAXLENGTH(100)]}
+                            onInput={inputHandler}
+                        />
+                        <SearchProduct
+                            product={product}
+                            setProduct={setProduct}
+                            onInputHandler={inputHandler}
+                            onSelectHandler={selectHandler}
+                            name={formState.inputs.name.value}
+                        />
+                        <Input
+                            label="Count:"
+                            id="count"
+                            placeholder="Count:"
+                            errorText="Maximum quantity 1000"
+                            validators={[VALIDATOR_MAX(1000)]}
+                            initialValue={"0"}
+                            initialValid={true}
+                            onInput={inputHandler}
+                            type="number"
+                            min="0"
+                            max="1000"
+                        />
+                        <Input
+                            label="Weight"
+                            id="weight"
+                            placeholder="Weight in grams"
+                            errorText="Maximum weight 1000000"
+                            validators={[VALIDATOR_MAX(1000000)]}
+                            onInput={inputHandler}
+                            initialValid={true}
+                            initialValue={"0"}
+                            type="number"
+                            min="0"
+                            max="1000000"
+                        />
+                        <Button type="submit" disabled={!formState.isValid} colorScheme="gray"
+                                color="var(--dark)">Add to list</Button>
+                    </VStack>
+                </form>}
         </>
     );
 };
--- src/lists/pages/ItemsInList.tsx
+++ src/lists/pages/ItemsInList.tsx
@@ -1,54 +1,57 @@
-import {GetListResponse, ProductCategory} from 'interfaces';
-import React, {useEffect, useState} from 'react';
-import {ItemsList} from "../components/ItemInList/ItemsList";
-import {useParams} from "react-router-dom";
-import {useDispatch, useSelector} from "react-redux";
-import {setItemsInList} from "../../common/Redux/actions/list";
-import {useHttpClient} from "../../common/hooks/http-hook";
-import {AddItem} from "../components/ItemInList/AddItem";
-import {RootState} from "../../common/Redux/store";
+import { ProductCategory } from "interfaces";
+import React, { useEffect } from "react";
+import { ItemsList } from "../components/ItemInList/ItemsList";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { setItemsInList } from "../../common/Redux/actions/list";
+import { useHttpClient } from "../../common/hooks/http-hook";
+import { AddItem } from "../components/ItemInList/AddItem";
+import { RootState } from "../../common/Redux/store";
+import { InfoModal } from "../../common/components/UiElements/InfoModal";
+import { LoadingSpinner } from "../../common/components/UiElements/LoadingSpinner";
+import { Section } from "../../common/components/UiElements/Section";
 
 export const ItemsInList = () => {
-    const {isLoading, error, sendRequest, clearError} = useHttpClient();
-    const {id,name} = useParams()
-    const dispatch = useDispatch()
+    const { isLoading, error, sendRequest, clearError } = useHttpClient();
+    const { id, name } = useParams();
+    const dispatch = useDispatch();
     const entries = Object.entries(ProductCategory);
     const category = [];
-    const {list} = useSelector((store: RootState) => store.lists)
+    const { list } = useSelector((store: RootState) => store.lists);
 
     useEffect(() => {
         (async () => {
-           const list = await sendRequest(`/list/user/${id}`)
-            dispatch(setItemsInList(list))
-        })()
-    },[id])
+            const list = await sendRequest(`/list/user/${id}`);
+            dispatch(setItemsInList(list));
+        })();
+    }, [id]);
 
     for (const key of entries) {
-        if (typeof key[1] === 'number') {
+        if (typeof key[1] === "number") {
             category.push(key[0]);
         }
     }
 
-    //@TODO added error modal
-    //@TODO added loading spinner
-    if (list === undefined || isLoading){
-        return <p>Loading...</p>
-    }
     return (
-        <div className="section">
-            <h2>Dodaj produkt do listy</h2>
-            <AddItem/>
-            <h2>Lista {name}</h2>
-            <div>
-                <ul>
-                    {category.map((category, id) => (<ItemsList
-                        key={id}
-                        categoryId={id}
-                        list={list}
-                        categoryName={category}
-                    />))}
-                </ul>
-            </div>
-        </div>
+        <>
+            {error &&
+                <InfoModal message={error} isError onClose={clearError} title={"Failed!"}/>}
+            {isLoading && <LoadingSpinner/>}
+            <Section>
+                <h2>Add product to list</h2>
+                <AddItem/>
+                <h2>List {name}</h2>
+                <div>
+                    <ul>
+                        {category.map((category, id) => (<ItemsList
+                            key={id}
+                            categoryId={id}
+                            list={list}
+                            categoryName={category}
+                        />))}
+                    </ul>
+                </div>
+            </Section>
+        </>
     );
 };
