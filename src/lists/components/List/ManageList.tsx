8c562dc0afdec73bed2af06d41808035449ecde8
Marcin Biłka <98228475+BilkaDev@users.noreply.github.com>
2022-09-14 14:20:53 +0200

    Translate text eng.

--- src/lists/components/List/ManageList.tsx
+++ src/lists/components/List/ManageList.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import {VALIDATOR_MAXLENGTH, VALIDATOR_MINLENGTH} from "../../../common/utils/validators";
-import {Input} from "../../../common/components/FormElements/Input";
+import React from "react";
+import { VALIDATOR_MAXLENGTH, VALIDATOR_MINLENGTH } from "../../../common/utils/validators";
+import { Input } from "../../../common/components/FormElements/Input";
 
 
 interface Props {
     inputHandler: (id: string, value: string, isValid: boolean) => void;
-    initialValue?: {name: string};
+    initialValue?: { name: string };
     initialValid?: boolean;
 }
 
 export const ManageList = (props: Props) => {
-    const {inputHandler,initialValid,initialValue} = props;
+    const { inputHandler, initialValid, initialValue } = props;
     return (
         <>
             <Input
-                label="Nazwa"
+                label="Name"
                 id="name"
-                placeholder="Nazwa listy"
-                errorText="Nazwa produktu jest wymagana (min. 2 znaki max. 100)."
+                placeholder="List name"
+                errorText="Product name is required (min. 2 characters max. 100)."
                 validators={[VALIDATOR_MINLENGTH(2), VALIDATOR_MAXLENGTH(100)]}
                 onInput={inputHandler}
                 initialValid={initialValid}
