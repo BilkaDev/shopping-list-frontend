fc30135b167c39826b12466e84c0f71fd32a52cb
Marcin Biłka <98228475+BilkaDev@users.noreply.github.com>
2022-11-29 15:46:33 +0100

    Added react form to component edit password form.

--- src/common/components/Header/EditPasswordForm.tsx
+++ src/common/components/Header/EditPasswordForm.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-import { useFormik } from 'formik';
 import {
   Button,
   FormControl,
@@ -13,22 +12,31 @@ import * as Yup from 'yup';
 import { useHttpClient } from '../../hooks/http-hook';
 import { LoadingSpinner } from '../UiElements/LoadingSpinner';
 import { InfoModal } from '../UiElements/InfoModal';
+import { ChangePasswordResponse } from 'interfaces';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup/dist/yup';
 
-const EditSchema = Yup.object().shape({
+const ChangePasswordSchema = Yup.object().shape({
   password: Yup.string()
     .min(6, 'Password is too short! minimum length is 6 characters!')
     .max(255, 'Password is too long! Maximum length is 255 characters!')
     .required('Required!'),
-  'new-password': Yup.string()
+  newPassword: Yup.string()
     .min(6, 'Password is too short! minimum length is 6 characters!')
     .max(255, 'Password is too long! Maximum length is 255 characters!')
     .required('Required!'),
-  'new-password-repeat': Yup.string()
+  newPasswordRepeat: Yup.string()
     .min(6, 'Password is too short! minimum length is 6 characters!')
     .max(255, 'Password is too long! Maximum length is 255 characters!')
     .required('Required!')
-    .oneOf([Yup.ref('new-password'), null], 'Passwords must match.'),
+    .oneOf([Yup.ref('newPassword'), null], 'Passwords must match.'),
 });
+
+interface EditPasswordFormInputs {
+  password: string;
+  newPassword: string;
+  newPasswordRepeat: string;
+}
 
 export const EditPasswordForm = () => {
   const { sendRequest, error, clearError, isLoading } = useHttpClient();
@@ -42,34 +50,32 @@ export const EditPasswordForm = () => {
     status: undefined,
   });
   const toast = useToast();
-  const formik = useFormik({
-    initialValues: {
-      password: '',
-      'new-password': '',
-      'new-password-repeat': '',
-    },
-    validationSchema: EditSchema,
-    onSubmit: async values => {
-      // const data = await sendRequest('/user/edit', 'PATCH', {
-      //     pwd: values.password,
-      //     newPwd: values["new-password"],
-      // });
-      // if (data.isSuccess) {
-      //     setToastMessage({
-      //         status: 'success',
-      //         title: 'Success!',
-      //         body: 'The password has been changed.',
-      //     })
-      // } else {
-      //     setToastMessage({
-      //         status: 'error',
-      //         title: 'Failed!',
-      //         body: 'Problem when changing password, try again.',
-      //     })
-      //     formik.values.password = ''
-      // }
-    },
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<EditPasswordFormInputs>({
+    resolver: yupResolver(ChangePasswordSchema),
   });
+
+  async function onSubmit(values: EditPasswordFormInputs) {
+    const data = await sendRequest<ChangePasswordResponse>(
+      '/user/edit',
+      'POST',
+      {
+        newPwd: values.newPasswordRepeat,
+        pwd: values.password,
+      }
+    );
+    if (data) {
+      setToastMessage({
+        status: 'success',
+        title: 'Success!',
+        body: 'The password has been changed.',
+      });
+    }
+  }
 
   useEffect(() => {
     if (toastMessage.title !== '' && toastMessage.body !== '') {
@@ -96,59 +102,46 @@ export const EditPasswordForm = () => {
           title={'Failed!'}
         />
       )}
-      <form onSubmit={formik.handleSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <VStack spacing={4} align="flex-start">
-          <FormControl isInvalid={!!formik.errors.password}>
+          <FormControl isInvalid={!!errors.password}>
             <Input
-              id="password"
-              name="password"
+              {...register('password')}
               type="password"
               placeholder="Password"
               variant="filled"
               bgColor="#292A2B"
               color="#DADADA"
-              onChange={formik.handleChange}
-              value={formik.values.password}
             />
-            {!!formik.errors.password && (
-              <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
-            )}
+            <FormErrorMessage>
+              {errors.password && errors.password.message}
+            </FormErrorMessage>
           </FormControl>
-          <FormControl isInvalid={!!formik.errors['new-password']}>
+          <FormControl isInvalid={!!errors.newPassword}>
             <Input
-              id="new-password"
-              name="new-password"
+              {...register('newPassword')}
               type="password"
               placeholder="New password"
               variant="filled"
               bgColor="#292A2B"
               color="#DADADA"
-              onChange={formik.handleChange}
-              value={formik.values['new-password']}
             />
-            {!!formik.errors['new-password'] && (
-              <FormErrorMessage>
-                {formik.errors['new-password']}
-              </FormErrorMessage>
-            )}
+            <FormErrorMessage>
+              {errors.newPassword && errors.newPassword.message}
+            </FormErrorMessage>
           </FormControl>
-          <FormControl isInvalid={!!formik.errors['new-password-repeat']}>
+          <FormControl isInvalid={!!errors.newPasswordRepeat}>
             <Input
-              id="new-password-repeat"
-              name="new-password-repeat"
+              {...register('newPasswordRepeat')}
               type="password"
               placeholder="Repeat new password"
               variant="filled"
               bgColor="#292A2B"
               color="#DADADA"
-              onChange={formik.handleChange}
-              value={formik.values['new-password-repeat']}
             />
-            {!!formik.errors['new-password-repeat'] && (
-              <FormErrorMessage>
-                {formik.errors['new-password-repeat']}
-              </FormErrorMessage>
-            )}
+            <FormErrorMessage>
+              {errors.newPasswordRepeat && errors.newPasswordRepeat.message}
+            </FormErrorMessage>
           </FormControl>
           <Stack spacing={10} width="100%" pt="10px">
             <Stack direction={{ base: 'column', sm: 'row' }} align={'center'}>
